4d052ff8fc41b23c01f63b536d198896bea1c18b
Pito Salas <pitosalas@gmail.com>
2018-10-02 12:16:27 -0400

    Checkpoint work to git

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ApplicationRecord
+  has_many :students :through :student_course
+  has_many :student_course
+  has_
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_155441) do
+ActiveRecord::Schema.define(version: 2018_09_28_191528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2018_09_28_155441) do
     t.string "name"
     t.string "desc"
     t.string "prof"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
